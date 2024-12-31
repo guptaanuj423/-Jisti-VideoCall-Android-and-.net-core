5bed3ec1c1122da885d7502a3150bc02b1b4c400
Anuj Gupta <89540363+guptaanuj423@users.noreply.github.com>
2024-12-31 16:49:45 +0530

    Add files via upload

---
+++ JitsiVideoCalling(.Net web)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JitsiVideoCalling.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
